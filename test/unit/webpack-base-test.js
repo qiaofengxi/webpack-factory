15692a771fdee7d137500e692f6eef6f2941854f
jiangzhefeng <zhefengjiang@easyops.cn>
2019-10-06 20:27:49 +0800

    test(): fix test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,6 +7,6 @@ describe('webpack.base.js test case', () => {
     it('entry', () => {
 
         assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js') > -1, true);
-        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js'), true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
     });
 });
